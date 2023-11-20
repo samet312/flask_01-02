7d60091cbe46e4c7d90c2b364628953426ef4284
Samet BÜLBÜL <samet312@gmail.com>
2023-11-20 22:36:42 +0300

    hello world yazıldı

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,16 @@
 from flask import Flask 
 app = Flask(__name__)
+@app.route('/')
+def head():
+    return 'Hello World!'
+@app.route('/second')
 
+def second():
+    return 'This is the second page'
+@app.route('/third/subthird')
+
+def third():
+    return 'This is the subpage of third page'
 
 
 
