fe18fed8b62b9819b76da1bc10e3d17ae4be4e27
Samet BÜLBÜL <samet312@gmail.com>
2023-11-20 23:13:53 +0300

    son ornek ok

--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    
+    return render_template('index.html', number1=1234, number2=7896)
 
+@app.route('/cohort16')
+def cohort():
+    value1, value2 = 23, 45
+    return render_template('body.html',val1=value1 , val2=value2 ,sum = value1+value2)
 
 
 
